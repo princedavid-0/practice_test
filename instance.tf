3178290360c0279d79469fd4f3139b698a10b17b
Prince David <erhiakporehprince@yahoo.com>
2021-12-21 23:16:42 +0000

    removed key-pair

--- instance.tf
+++ instance.tf
@@ -9,7 +9,7 @@ resource "aws_launch_configuration" "web" {
   associate_public_ip_address = true
 
   user_data           = file("install_httpd.sh")
-  
+
   lifecycle {
     create_before_destroy = true
   }
@@ -17,5 +17,5 @@ resource "aws_launch_configuration" "web" {
 
 resource "aws_key_pair" "web_test" {
   key_name   = "my-keypair"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDQLxldIyJMwoNRo8+iCtaRrJtFEKQXqYbl4kCZ6Vav+xllzz+JJJRkDJ4ibG2hHujLM7pbEHH+DXsirVYPR8fk6P6Ihs5rhXycgyPKXdv2rQP5Tvp1RN9U2k02kg67HjaUfTWpBQvYIfI/XpkRRjXyy4ldL+jBjyVFIaVoP8QwwYrRQEChYzbzNAA/oO8bSVSiWUfzNtZ9O2k/HWBiFzf/sBozwTBMRxitL3BTcxDrm2klP45TmMskMc4wLz9o6olr/f1ExHEsFF1BdccswrS8jWzQo9u+a3HGHCd3H5xU4BGzYzxE1K8OVGZrfjCzlToLpGqjIsJr5Fg5/YmGmDEeqijoXSrIzaL9UOmtNCCjFaAEAJF8yKWUz6vodeLdo1gosNsCNsrO2f7d586yo0jCAEKQjAPs7EAWO3TqMbyMQzfnMnpQNS30WZ6w5NmVUrpGYON6wjd7CnKBq4p9MtUx7EDntzFL7AFlsKTyzbYLNt9zzT7/qPvxvKKMRaRxSjM= user@PrinceDavidMBP"
+  public_key = "<insert_public_key>"
 }
